6bb651a3186657eede5be36a949c4036047f6fdf
Fabio Payão <fabio.payao@indicium.tech>
2024-08-07 18:32:48 +0000

    Creating my first staging

---
+++ models/staging/sap/stg_sap__sales_person.sql
@@ -0,0 +1,17 @@
+with
+    rennamed as (
+        select
+            cast(businessentityid as int) as pk_sales_person
+            , cast(territoryid as int) as territory_id
+            , cast(salesquota as int) as sales_quota
+            , cast(bonus as int) as bonus_sales_person
+            , cast(commissionpct as float) as commission_pct_sales_person
+            , cast(salesytd as float) as sales_ytd_sales_person
+            , cast(saleslastyear as float) as sales_last_year_sales_person
+            , cast(modifieddate as timestamp) as modified_date_sales_person
+            -- rowguid
+        from {{ source('sap', 'salesperson') }}
+    )
+
+select *
+from rennamed
